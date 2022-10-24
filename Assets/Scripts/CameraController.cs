33b1a003d6c4c390e4e8e515b877d5a58fbd90f8
ArchMag1cs42 <pandarienne@gmail.com>
2022-10-24 11:39:39 -0400

    atrium collider and camera

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = player.transform.position - this.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = player.transform.position - offset;
+    }
+}
